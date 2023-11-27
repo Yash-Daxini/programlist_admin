35ad58f90e90be8ce6fea2f560b85458288c2f98
Infinity <200540107113@darshan.ac.in>
2023-11-27 07:02:36 +0530

    Made table Responsive

--- src/Components/SelectByID.js
+++ src/Components/SelectByID.js
@@ -9,10 +9,10 @@ const SelectByID = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if( sessionStorage.getItem("user") === null ){
+    if (sessionStorage.getItem("user") === null) {
       navigate("../login");
     }
-  }, [navigate])
+  }, [navigate]);
 
   useEffect(() => {
     fetch("https://programlist-backend.onrender.com/programs/" + params.id)
@@ -28,50 +28,40 @@ const SelectByID = () => {
   return (
     <div className="align-items-center my-5">
       <h1 className="my-5">{programObj.program_name}</h1>
-      {/* <h5>
-        {programObj.issolved ? (
-          <span className="text-success">
-            {" "}
-            <ion-icon name="checkmark-circle-sharp"></ion-icon>{" "}
-          </span>
-        ) : (
-          <span></span>
-        )}
-      </h5> */}
       <div className="my-5 d-flex justify-content-center align-items-center flex-column">
-        <h4 style={{textTransform:"capitalize"}}>Topic : {programObj.program_Topic} </h4>
-        <hr/>
+        <h4 style={{ textTransform: "capitalize" }}>
+          Topic : {programObj.program_topic}{" "}
+        </h4>
+        <hr />
         {programObj.difficulty === "Easy" ? (
-            <h4 className="customBadgeSuccess">
-              <span className="fs-6">{programObj.difficulty}</span>
-            </h4>
-          ) : programObj.difficulty === "Medium" ? (
-            <h4 className="customBadgeWarning">
-              <span className="fs-6">{programObj.difficulty}</span>
-            </h4>
-          ) : (
-            <h4 className="customBadgeDanger">
-              <span className="fs-6">{programObj.difficulty}</span>
-            </h4>
-          )}
+          <h4 className="customBadgeSuccess">
+            <span className="fs-6">{programObj.difficulty}</span>
+          </h4>
+        ) : programObj.difficulty === "Medium" ? (
+          <h4 className="customBadgeWarning">
+            <span className="fs-6">{programObj.difficulty}</span>
+          </h4>
+        ) : (
+          <h4 className="customBadgeDanger">
+            <span className="fs-6">{programObj.difficulty}</span>
+          </h4>
+        )}
       </div>
       <div className="my-5 d-flex justify-content-center align-items-center">
-        <button className="btn btn-outline-primary mx-5">
-          <Link
-            to={programObj.program_link}
-            className="text-decoration-none text-dark"
-          >
-            Solve Here{" "}
-          </Link>
-        </button>
-        <button className="btn btn-outline-primary mx-5">
-          <Link
-            to={programObj.program_solutionlink}
-            className="text-decoration-none text-dark"
-          >
-            See Solution{" "}
-          </Link>
-        </button>
+        <Link
+          to={programObj.program_link}
+          target={"_blank"}
+          className="text-decoration-none"
+        >
+          <button className="btn btn-outline-dark mx-5">Solve Here </button>
+        </Link>
+        <Link
+          to={programObj.program_solutionlink}
+          className="text-decoration-none"
+          target={"_blank"}
+        >
+          <button className="btn btn-outline-dark mx-5">See Solution </button>
+        </Link>
       </div>
     </div>
   );
